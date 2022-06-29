e103c0cb3b36e4cf0a74fe92b90c164c881ef84e
nkpn <illinmikita@gmail.com>
2022-06-29 22:08:11 +0300

    ап

---
+++ wordpress-theme/index.php
@@ -0,0 +1,93 @@
+<?php get_header(); ?>
+<body>
+      <section class="hero-section">
+
+      </section>
+
+      <section class="offer-section">
+        <div class="container">
+          <h3>our exquisite sushi offfer</h3>
+          <p class="description">Japan is well known for its wide range of fantastic cuisine, and the most popular Japanese dishes, such as ramen and sushi, are easily found in restaurants all over the world. But AKAI TORI is one style of Japanese food you may not have heard of before. </p>
+          <ul class="food-list flex">
+            <li class="list-item">
+              <img src="./images/soup.svg" alt="Soup">
+              <h4>started</h4>
+              <p>Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+            <li class="list-item">
+              <img src="./images/shrimp.svg" alt="Shrimp">
+              <h4>MAKI ROLLS</h4>
+              <p>Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+            <li class="list-item">
+              <img src="./images/sushi.svg" alt="sushi">
+              <h4>SUSHI</h4>
+              <p>Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="section-take-food">
+        <div class="flex">
+          <div class="image-wrapper">
+            <img src="./images/take-any-food.png" alt="Take any food!">
+          </div>
+          <div class="take-any-food">
+            <h3>Take any food</h3>
+            <p>Japan is well known for its wide range of fantastic cuisine, and the most popular Japanese dishes, such as ramen and sushi, are easily found in restaurants all over the world. But AKAI TORI is one style of Japanese food you may not have heard of before. </p>
+            <a href="" class="button">BUTTON</a>
+          </div>
+        </div>
+      </section>
+
+      <section class="section-our-story">
+        <div class="container">
+          <div class="heading-wrapper">
+            <h3>our awesome story</h3>
+          </div>  
+          <ul class="carousel-list">
+            <li class="carousel-item">
+              <img src="./images/story-template.png" alt="our story">
+              <h4 class="card-heading">SUSHI</h4>
+              <p class="card-description">Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+            <li class="carousel-item">
+              <img src="./images/story-template.png" alt="our story">
+              <h4 class="card-heading">SUSHI</h4>
+              <p class="card-description">Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+            <li class="carousel-item">
+              <img src="./images/story-template.png" alt="our story">
+              <h4 class="card-heading">SUSHI</h4>
+              <p class="card-description">Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+            <li class="carousel-item">
+              <img src="./images/story-template.png" alt="our story">
+              <h4 class="card-heading">SUSHI</h4>
+              <p class="card-description">Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+            <li class="carousel-item">
+              <img src="./images/story-template.png" alt="our story">
+              <h4 class="card-heading">SUSHI</h4>
+              <p class="card-description">Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+            <li class="carousel-item">
+              <img src="./images/story-template.png" alt="our story">
+              <h4 class="card-heading">SUSHI</h4>
+              <p class="card-description">Japanese food, where the dining experience is not only about the actual food consumed</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section class="section-food-carousel">
+        <ul class="food-carousel">
+          <li><img src="./images/carousel-1.png" alt="asian food"></li>
+          <li><img src="./images/carousel-2.png" alt="asian food"></li>
+          <li><img src="./images/carousel-3.png" alt="asian food"></li>
+          <li><img src="./images/carousel-4.png" alt="asian food"></li>
+        </ul>
+      </section>
+      <?php wp_footer(); ?>
+    </body>
